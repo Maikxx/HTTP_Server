013da24eedc931ead6f6b7d4c0179e7db9e65e6c
Maikel van Veen <maikeloo10@hotmail.com>
2018-02-26 23:19:26 +0100

    feat(*): add boilerplate with two tasks completed, normal routing and 404s

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # HTTP_Server
-(Draft) Study - A standard HTTP server with Node.js
+
+A standard HTTP server with Node.js
+
+## Features
+
+* 200 OK and contents if valid HTML files are asked (index.html & about.html).
+* Handle 404 route.
---
+++ server.js
@@ -0,0 +1,57 @@
+const fs = require('fs');
+const path = require('path');
+const http = require('http');
+const mimeTypesLookup = require('mime-types').lookup;
+
+const server = http.createServer(handleRequest);
+const exportDirName = 'public';
+
+const supportedMimeTypes = ['text/html', 'text/css', 'image/x-icon', 'image/jpeg', 'image/png'];
+
+function handleNotFounds (routeURL, response) {
+    fs.readFile(path.join(exportDirName, '/not_found.html'), (error, buffer) => {
+        if (error) {
+            throw new Error(error);
+        }
+
+        response.statusCode = 404;
+        response.setHeader('Content-Type', 'text/html');
+        response.end(buffer);
+    });
+}
+
+function handleRequest (request, response) {
+    let route = request.url;
+    const type = mimeTypesLookup(route);
+
+    if (route === '/') {
+        route = '/index.html';
+    }
+
+    fs.readFile(path.join(exportDirName, route), (error, buffer) => {
+        // If the route url given does not correspond with the supported mime types, give back a access restriction error.
+        if (error || !supportedMimeTypes.includes(type)) {
+            handleNotFounds(route, response);
+        } else {
+            response.statusCode = 200;
+            response.setHeader('Content-Type', type);
+            response.end(buffer);
+        }
+    });
+}
+
+server.on('listening', () => {
+    console.log('Ok. Server is running!');
+});
+
+server.listen(8000);
+
+// Events for closing the server.
+process.on('uncaughtException', () => {
+    server.close();
+});
+
+// // Events for closing the server.
+process.on('SIGTERM', () => {
+    server.close();
+});
